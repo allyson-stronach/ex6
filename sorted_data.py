784e6b934ba28195689988f5b5c285b03c2c31e5
Hackbright Student <info@hackbrightacademy.com>
2014-10-06 16:31:16 -0700

    Finished ex6 final working code

--- sorted_data.py
+++ sorted_data.py
@@ -1,55 +1,20 @@
-# with open("scores.txt") as restfile:
-#     rest_scores = restfile.readlines()
-
-# print rest_scores
-
-
 rest_scores = open("scores.txt")
 restaurant = {}
 
 for line in rest_scores:
+    #strip line by \n line
     line = line.rstrip()
+    #split data into a list by colon
     entries = line.split(":")
     restaurant_names = entries[0]
     restaurant_scores = int(entries[1])
+    #append data into the dictionary using variables above
     restaurant[restaurant_names] = restaurant_scores
-
-print restaurant
-
-
-
-# while line:
-#     values = line.split('\n')
-    
-#     print values
-#     line = rest_scores.read()
-
-# rest_scores.close()
-
-# # rest_scores = open("scores.txt", 'r')
-# restaurant_entries = line.split(':')
-
-   # restaurant_score = restaurant_entries[1]
-
-# print "%s was rated %r." % ()
-
-
-
-
-
-
-    #need to strip \n char from rest list
-
-
-
-#open and reads file
-#strip data
-#split data into a list by colon
-#generate a dictionary of restaurants by scores
 
 
 #spits out ratings in alph order, by restaurant
-
+for key, value in sorted(restaurant.iteritems()):
+    print "%s was rated %r." % (key, value)
 
 
 
