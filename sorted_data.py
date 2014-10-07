0e6ad459a4762bcd945755f1a00f782ae1ee96e5
Hackbright Student <info@hackbrightacademy.com>
2014-10-06 17:24:20 -0700

    Refactored code after code review

--- sorted_data.py
+++ sorted_data.py
@@ -1,21 +1,27 @@
-rest_scores = open("scores.txt")
-restaurant = {}
+#opening text without hard coding it into our program
+txt = open("scores.txt")
 
-for line in rest_scores:
-    #strip line by \n line
-    line = line.rstrip()
-    #split data into a list by colon
-    entries = line.split(":")
-    restaurant_names = entries[0]
-    restaurant_scores = int(entries[1])
-    #append data into the dictionary using variables above
-    restaurant[restaurant_names] = restaurant_scores
+def main(txt):
+    #rest_scores = open("scores.txt")
+    restaurant = {}
 
+    for line in txt:
+        #strip line by \n line
+        line = line.rstrip()
+        #split data into a list by colon
+        entries = line.split(":")
+        restaurant_name = entries[0]
+        restaurant_score = int(entries[1])
+        #append data into the dictionary using variables above
+        restaurant[restaurant_name] = restaurant_score
 
-#spits out ratings in alph order, by restaurant
-for key, value in sorted(restaurant.iteritems()):
-    print "%s was rated %r." % (key, value)
+    for key in sorted(restaurant.keys()):
+        print "%s was rated %r." % (key, restaurant[key])
+    #spits out ratings in alph order, by restaurant
+    # for key, value in sorted(restaurant.iteritems()):
+    #     print "%s was rated %r." % (key, value)
 
-
+if __name__ == "__main__":
+    main(txt)
 
 
